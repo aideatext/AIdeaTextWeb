b4eeabc2b16f56a09bbd06220883127e27d502f8
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-02-26 00:32:39 -0600

    Update script.js

--- test_0/script.js
+++ test_0/script.js
@@ -17,28 +17,17 @@ async function loadVisualize(pyodide) {
 }
 
 async function main() {
-  // Set default value for input
-  document.getElementById("input").value = "Hello! Guess what? I'm running Python in your browser!\n\nMy name is Clara and I live in Berkeley, California.";
-
-  // Load Pyodide and packages
   let pyodide = await loadPyodide();
-
   await loadPackages(pyodide);
-
-  // Load visualize function
   let vis_fn = await loadVisualize(pyodide);
 
-  // Enable the form button and change label
   document.getElementById("submit").disabled = false;
   document.getElementById("submit").innerHTML = "Visualize";
 
-  // Add event listener to form
   document.getElementById("form").addEventListener("submit", async (e) => {
     e.preventDefault();
-
     let input = document.getElementById("input").value;
     let html = await vis_fn(input);
-
     document.getElementById("output").innerHTML = html;
   });
 }
