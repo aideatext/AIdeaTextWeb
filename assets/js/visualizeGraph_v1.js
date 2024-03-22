0c6c89368ca2026c28c03f7a9ab2dc36c3634106
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-03-21 22:41:25 -0600

    Update visualizeGraph_v1.js

--- assets/js/visualizeGraph_v1.js
+++ assets/js/visualizeGraph_v1.js
@@ -1,6 +1,6 @@
 function processText() {
     var textInput = document.getElementById("text").value;
-    fetch('https://5f6b6akff7.execute-api.us-east-2.amazonaws.com/DEV/AIdeaTextdisplaCy,{
+    fetch('https://5f6b6akff7.execute-api.us-east-2.amazonaws.com/DEV/AIdeaTextdisplaCy', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
