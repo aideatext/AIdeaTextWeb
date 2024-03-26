2c2a57a215c4371e810b710b44928c1fa0beee55
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-03-26 14:20:33 -0600

    Update visualizeGraph_v8.js

--- assets/js/visualizeGraph_v8.js
+++ assets/js/visualizeGraph_v8.js
@@ -1,3 +1,29 @@
+function processText() {
+    var textInput = document.getElementById("text").value;
+    if (!textInput.trim()) {
+        console.error("El texto para analizar no puede estar vacío.");
+        return; // Detener la ejecución si el texto está vacío
+    }
+
+    fetch('https://5f6b6akff7.execute-api.us-east-2.amazonaws.com/DEV/AIdeaTextdisplaCy', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: textInput }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Datos recibidos del backend:", data);
+        // Visualiza la sintaxis del texto en la página web
+        visualizeGraph(data); // Asegúrate de que esta línea esté presente
+    })
+    .catch(error => {
+        console.error("Error al procesar el texto:", error)
+    });
+    }
+/////////////////////////////////////////////////////////////////////////////////////
+
 function visualizeGraph(data) {
     // Asegúrate de que este elemento exista en tu HTML
     const graphData = data.graph;
