f2806ca15c22d0441d034f6c642c331418a1daec
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-03-30 21:55:04 -0600

    Create visualizeGraph-v14-000.js

---
+++ assets/js/visualizeGraph-v14-000.js
@@ -0,0 +1,209 @@
+/**
+ * Obtiene un elemento del DOM por su ID.
+ * @param {string} id - El ID del elemento.
+ * @returns {HTMLElement} - El elemento del DOM.
+ */
+function getContainerElement(id) {
+    return document.getElementById(id);
+}
+
+/**
+ * Limpia el contenido de un contenedor.
+ * @param {HTMLElement} container - El contenedor a limpiar.
+ */
+function clearContainer(container) {
+    container.innerHTML = '';
+}
+
+/**
+ * Encuentra la palabra más común en un array de palabras.
+ * @param {Array} words - El array de palabras.
+ * @returns {string} - La palabra más común.
+ */
+function findMostCommonWordInText(words) {
+    const wordFrequency = {};
+    
+    // Contar la frecuencia de cada palabra
+    words.forEach(word => {
+        const normalizedWord = word.toLowerCase(); // Convertir la palabra a minúsculas para evitar distinciones de mayúsculas y minúsculas
+        wordFrequency[normalizedWord] = (wordFrequency[normalizedWord] || 0) + 1;
+    });
+    
+    // Encontrar la palabra con la frecuencia más alta
+    let mostCommonWord = '';
+    let highestFrequency = 0;
+    
+    for (const word in wordFrequency) {
+        if (wordFrequency[word] > highestFrequency) {
+            mostCommonWord = word;
+            highestFrequency = wordFrequency[word];
+        }
+    }
+    
+    return mostCommonWord;
+}
+
+/**
+ * Encuentra la palabra más común en un conjunto de nodos.
+ * @param {Array} nodes - Los nodos a analizar.
+ * @returns {Object} - El nodo más común.
+ */
+function findMostCommonWord(nodes) {
+    return nodes.reduce((max, node) => {
+        return node.frequency > max.frequency ? node : max;
+    }, nodes[0]);
+}
+
+
+/**
+ * Encuentra la palabra menos común en un conjunto de nodos.
+ * @param {Array} nodes - Los nodos a analizar.
+ * @returns {Object} - El nodo menos común.
+ */
+function findLeastCommonWord(nodes) {
+    return nodes.reduce((min, node) => {
+        return node.frequency < min.frequency ? node : min;
+    }, nodes[0]);
+}
+
+/**
+ * Procesa el texto ingresado.
+ */
+
+function processText() {
+    var textInput = document.getElementById("text-1").value;
+    if (!textInput.trim()) {
+        console.error("El texto para analizar no puede estar vacío.");
+        return; // Detener la ejecución si el texto está vacío
+    }
+
+    fetch('https://5f6b6akff7.execute-api.us-east-2.amazonaws.com/DEV/callmodel', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: textInput }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Datos recibidos del backend:", data);
+        if (data.syntax && data.syntax.nodes) {
+        
+        // Visualiza la sintaxis del texto en la página web
+        visualizeGraph(data); // Asegúrate de que esta línea esté presente
+      } else {  
+        console.error("Error: No se encontraron datos de análisis sintáctico válidos en la respuesta del servidor.");
+        }
+    })
+    .catch(error => {
+        console.error("Error al procesar el texto:", error)
+    });
+}
+
+/**
+ * Visualiza los datos recibidos del backend.
+ * @param {Object} data - Los datos recibidos del backend.
+ */
+function visualizeGraph(data) {
+    const syntaxNetworkContainer = document.getElementById("syntax-network");
+    const semanticNetworkContainer = document.getElementById("semantic-network");
+    syntaxNetworkContainer.innerHTML = '';
+    semanticNetworkContainer.innerHTML = '';
+
+    if (data.syntax) {
+        // Visualización del Análisis Sintáctico
+        visualizeSyntaxTreemap(data.syntax, syntaxNetworkContainer);
+    }
+
+    if (data.entities) {
+        // Visualización del Análisis Semántico (Grafo)
+        visualizeSemantic(data.entities, data.cra, semanticNetworkContainer);
+    }
+}
+
+// Función para asignar colores a las categorías gramaticales
+function getColorByPOS(pos) {
+    const colorMap = {
+        'adp': '#1f77b4',
+        'det': '#ff7f0e',
+        'adj': '#2ca02c',
+        'noun': '#d62728',
+        'propn': '#9467bd',
+        'pron': '#8c564b',
+        'verb': '#e377c2',
+        'sconj': '#7f7f7f',
+        'adv': '#bcbd22',
+        'aux': '#17becf',
+        'cconj': '#aec7e8'
+    };
+    return colorMap[pos] || 'lightblue';
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Visualiza el análisis semántico.
+ * @param {Object} semanticData - Los datos de análisis semántico.
+ * @param {HTMLElement} semanticNetworkContainer - El contenedor para mostrar la red semántica.
+ */
+function visualizeSemantic(semanticData, semanticNetworkContainer) {
+    // Limpiamos el contenedor antes de mostrar los resultados
+    semanticNetworkContainer.innerHTML = '';
+
+    // Visualizamos los resultados del análisis semántico
+    visualizeCRA(semanticData.semantic_analysis, semanticNetworkContainer);
+}
+
+/**
+ * Visualiza el análisis CRA.
+ * @param {Object} craData - Los datos de análisis CRA.
+ * @param {HTMLElement} semanticNetworkContainer - El contenedor para mostrar la red semántica.
+ */
+function visualizeCRA(craData, semanticNetworkContainer) {
+    // Limpiamos el contenedor antes de mostrar los resultados
+    semanticNetworkContainer.innerHTML = '';
+
+    // Configuración del contenedor SVG
+    const width = 1200;
+    const height = 800;
+    const svg = d3.select(semanticNetworkContainer).append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    // Escalador para asignar tamaños proporcionales a los nodos basados en su importancia
+    const scaleNodeSize = d3.scaleLinear()
+        .domain([0, d3.max(craData.nodes.map(node => node.weight))])
+        .range([5, 30]); // Tamaño del nodo entre 5 y 30 píxeles
+
+    // Creamos los nodos y los enlaces basados en los datos del CRA
+    const nodes = craData.nodes.map(node => ({ id: node.id, size: scaleNodeSize(node.weight) }));
+
+    // Creamos la simulación de fuerzas
+    const simulation = d3.forceSimulation(nodes)
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+    // Dibujamos los nodos
+    const node = svg.selectAll("circle")
+        .data(nodes)
+        .enter().append("circle")
+        .attr("r", d => d.size)
+        .attr("fill", "#66ccff"); // Color azul para los nodos
+
+    // Etiquetas de texto para los nodos
+    const text = svg.selectAll("text")
+        .data(nodes)
+        .enter().append("text")
+        .text(d => d.id)
+        .attr("x", 8)
+        .attr("y", "0.31em");
+
+    // Actualizamos la posición de los elementos en cada paso de la simulación
+    simulation.on("tick", () => {
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+        text
+            .attr("x", d => d.x + 10)
+            .attr("y", d => d.y);
+    });
+}
