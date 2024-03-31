29ad9187b9c890b7c6d6731fbc71ed9aca8ee131
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-03-31 10:08:31 -0600

    Update visualizeSemantic.js

--- assets/js/visualizeSemantic.js
+++ assets/js/visualizeSemantic.js
@@ -41,7 +41,7 @@ function semanticProcess() {
         return; // Detener la ejecución si el texto está vacío
     }
 
-    fetch('fetch('https://5f6b6akff7.execute-api.us-east-2.amazonaws.com/DEV/AIdeaTextdisplaCy', {
+    fetch('https://5f6b6akff7.execute-api.us-east-2.amazonaws.com/DEV/AIdeaTextdisplaCy', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
