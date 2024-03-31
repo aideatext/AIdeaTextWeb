b73018e29d323207d2dc851f66221c00918d9e0e
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-03-30 18:45:43 -0600

    Update visualizeGraph-v14.js

--- assets/js/visualizeGraph-v14.js
+++ assets/js/visualizeGraph-v14.js
@@ -224,8 +224,8 @@ function visualizeSyntaxTreemap(syntaxData, syntaxNetworkContainer) {
     cell.append("rect")
         .attr("width", d => d.x1 - d.x0)
         .attr("height", d => d.y1 - d.y0)
-        .attr("fill", "lightblue"); // Color de fondo de los rectángulos
-        // .attr("fill", d => getColorByPOS(d.data.name)); Obtener el color según la categoría gramatical
+        // .attr("fill", "lightblue"); Color de fondo de los rectángulos
+        .attr("fill", d => getColorByPOS(d.data.name)); Obtener el color según la categoría gramatical
 
     // Agregar etiquetas de texto a cada cuadrado del treemap
     cell.append("text")
@@ -276,7 +276,7 @@ function getColorByPOS(pos) {
         'aux': '#17becf',
         'cconj': '#aec7e8'
     };
-    return colorMap[pos] || '#000000'; // Color negro por defecto
+    return colorMap[pos] || 'lightblue'; // Color negro por defecto
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
