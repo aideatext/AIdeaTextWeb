95798fc7705f57ed588aaf99ae601073cbea2bc3
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-03-31 20:30:02 -0600

    Update visualizeSemantic.js

--- assets/js/visualizeSemantic.js
+++ assets/js/visualizeSemantic.js
@@ -54,7 +54,8 @@ function semanticProcess() {
 
         if (data.semantic_analysis && data.semantic_analysis.nodes && data.semantic_analysis.edges) {
             // Visualiza el análisis semántico en la página web
-            visualizeSemantic(data.semantic_analysis, semanticNetworkContainer);
+            // visualizeSemantic(data.semantic_analysis, semanticNetworkContainer);
+             visualizeGraph(data);
         }
     })
     .catch(error => {
