6f93a5c27b287208dc5639fd4fa9fc84cd6965d1
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-04-16 14:28:34 -0600

    Update visualizeDisplacy.js

--- assets/js/visualizeDisplacy.js
+++ assets/js/visualizeDisplacy.js
@@ -1,22 +1,15 @@
 // Contenedor para la red sintáctica
 const syntaxNetworkContainer = document.getElementById("syntax-network");
 
-/**
- * Limpia el contenido de un contenedor.
- * @param {HTMLElement} container - El contenedor a limpiar.
- */
 function clearContainer(container) {
     container.innerHTML = '';
 }
 
-/**
- * Procesa el texto ingresado para análisis sintáctico.
- */
 function syntaxProcess() {
     var textInput = document.getElementById("text-1").value;
     if (!textInput.trim()) {
         console.error("El texto para analizar no puede estar vacío.");
-        return; // Detener la ejecución si el texto está vacío
+        return;
     }
 
     fetch('https://5f6b6akff7.execute-api.us-east-2.amazonaws.com/DEV/callmodel', {
@@ -26,7 +19,7 @@ function syntaxProcess() {
         },
         body: JSON.stringify({ text: textInput }),
     })
-    .then(response => response.text()) // Cambiado de response.json() a response.text()
+    .then(response => response.text())
     .then(html => {
         console.log("HTML recibido del backend:", html);
         visualizeSyntax(html);
@@ -36,19 +29,13 @@ function syntaxProcess() {
     });
 }
 
-/**
- * Visualiza el HTML de Displacy recibido del backend.
- * @param {string} html - HTML del análisis sintáctico generado por Displacy.
- */
 function visualizeSyntax(html) {
     clearContainer(syntaxNetworkContainer);
     if (html) {
-        // Insertar directamente el HTML en el contenedor
         syntaxNetworkContainer.innerHTML = html;
     } else {
         console.error("No se recibieron datos válidos del servidor.");
     }
 }
 
-// Llamar a la función syntaxProcess al cargar la página
 syntaxProcess();
