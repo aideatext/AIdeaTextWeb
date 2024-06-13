928609676e64ae081b1dd9a1cb882b5b32ed9f6b
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-06-13 16:55:24 -0600

    Update mmt.js

--- assets/js/mmt.js
+++ assets/js/mmt.js
@@ -1,12 +1,14 @@
 // Agregado dentro de DOMContentLoaded para garantizar que el DOM esté cargado completamente
-// version 1x
+// v1xx
 document.addEventListener("DOMContentLoaded", function() {
     
-    const syntaxNetworkContainer = document.getElementById("syntax-network");
-    const syntaxButton = document.getElementById('syntaxButton');
+    const syntaxNetworkContainerEs = document.getElementById("syntax-network-es");
+    const syntaxNetworkContainerFr = document.getElementById("syntax-network-fr");
+    const translateButton = document.getElementById('syntaxButton');
     const progressBar = document.getElementById('progressBar');
+    const translationResult = document.getElementById('translation-result');
 
-    syntaxButton.addEventListener('click', syntaxProcess);
+    translateButton.addEventListener('click', syntaxProcess);
 
     function clearContainer(container) {
         if (container) {
@@ -31,9 +33,9 @@ document.addEventListener("DOMContentLoaded", function() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ text: textInput })
         })
-        .then(response => response.text())
-        .then(html => {
-                let progress = 0;
+        .then(response => response.json())
+        .then(data => {
+            let progress = 0;
             const interval = setInterval(() => {
                 progress += 10; // Incrementar progreso más lentamente
                 progressBar.style.width = progress + '%';
@@ -43,27 +45,40 @@ document.addEventListener("DOMContentLoaded", function() {
                     progressBar.style.width = '100%';
                     setTimeout(() => { progressBar.style.display = 'none'; }, 500);
 
-            if (html.trim().startsWith('<div')) {
-                clearContainer(syntaxNetworkContainer);
-                syntaxNetworkContainer.innerHTML = html;
-                console.log("Displacy output has been inserted into the container.");
-            } else {
-                console.error("No se recibieron datos válidos del servidor:", html);
-            }
-        }
+                    if (data.arc_diagram_es.trim().startsWith('<div')) {
+                        clearContainer(syntaxNetworkContainerEs);
+                        syntaxNetworkContainerEs.innerHTML = data.arc_diagram_es;
+                        console.log("Displacy output (ES) has been inserted into the container.");
+                    } else {
+                        console.error("No se recibieron datos válidos del servidor para el diagrama en español:", data.arc_diagram_es);
+                    }
+
+                    if (data.translated_text) {
+                        translationResult.innerText = `Texto traducido: ${data.translated_text}`;
+                    } else {
+                        console.error("No se recibió texto traducido del servidor");
+                    }
+
+                    if (data.arc_diagram_fr.trim().startsWith('<div')) {
+                        clearContainer(syntaxNetworkContainerFr);
+                        syntaxNetworkContainerFr.innerHTML = data.arc_diagram_fr;
+                        console.log("Displacy output (FR) has been inserted into the container.");
+                    } else {
+                        console.error("No se recibieron datos válidos del servidor para el diagrama en francés:", data.arc_diagram_fr);
+                    }
+                }
             }, 200); // Modifica este tiempo según la duración esperada del proceso
         
         })
         .catch(error => {
             console.error("Error al procesar el texto:", error);
-            clearInterval(interval);
             progressBar.style.width = '100%';
             progressBar.style.backgroundColor = 'red';
         });
     }
 
-    if (syntaxButton) {
-        syntaxButton.addEventListener('click', syntaxProcess);
+    if (translateButton) {
+        translateButton.addEventListener('click', syntaxProcess);
     } else {
         console.error("El botón de análisis sintáctico no se encuentra en el DOM.");
     }
