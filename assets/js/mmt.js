dc0c70c4f4b2578c7d16ff3a29edf542d6eeb8a9
Manuel Var Ale <158815600+aideatext@users.noreply.github.com>
2024-06-14 02:50:13 -0600

    Update mmt.js

--- assets/js/mmt.js
+++ assets/js/mmt.js
@@ -1,4 +1,4 @@
-// 123v
+// 1345v
 document.addEventListener("DOMContentLoaded", function() {
     const translatedTextContainer = document.getElementById("translated-text");
     const translateButton = document.getElementById('translateButton');
